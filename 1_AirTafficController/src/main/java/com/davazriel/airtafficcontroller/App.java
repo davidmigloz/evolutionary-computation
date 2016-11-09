f59ee3e163fffca8b7e52d31d562a6285d1a3d94
davidmigloz <me@davidmiguel.com>
2016-11-09 18:14:45 +0100

    Init project

---
+++ 1_AirTafficController/src/main/java/com/davazriel/airtafficcontroller/App.java
@@ -0,0 +1,10 @@
+package com.davazriel.airtafficcontroller;
+
+/**
+ * Start devplatform.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
